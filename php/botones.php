74dbe372180a7e18a1a2fa3425e116eeb5c54b60
Jacobo Santos <jsantoscores@gmail.com>
2021-03-10 18:18:27 +0100

    cambaiado botones

--- php/botones.php
+++ php/botones.php
@@ -4,7 +4,7 @@ echo ' <div class="row">
                <h5 class="text-secondary"> Bienvenido <small>' . $_SESSION["usuario"] . '</small> <h5>
             </div>
             <div class="col-6 text-right">
-               <a class="text-secondary" href="window.location.href=`inicio.php`" ><i class="fas fa-arrow-circle-left"></i></a>
+               <a class="text-secondary" href="inicio.php" ><i class="fas fa-arrow-circle-left"></i></a>
                <a href="cierre.php" class="text-secondary "> Cerrar Sesion</a>
             </div>
 </div>
