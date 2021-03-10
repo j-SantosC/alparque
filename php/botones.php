67d119e2b8bb44ad0c9315ce4120f675479a350f
Jacobo Santos <jsantoscores@gmail.com>
2021-03-10 18:13:26 +0100

    cambaiado botones

--- php/botones.php
+++ php/botones.php
@@ -1,11 +1,11 @@
 <?php
 echo ' <div class="row">
-<div class="col-sm-6 col-xs-12">
-   <h4 class="text-secondary"> Bienvenido <small>' . $_SESSION["usuario"] . '</small> <h3>
+<div class="col-sm-6 col-xs-6">
+   <h5 class="text-secondary"> Bienvenido <small>' . $_SESSION["usuario"] . '</small> <h5>
 </div>
-<div class="col-sm-6 col-xs-12 text-sm-right text-xs-center">
-    <button class="btn btn-outline-secondary" onClick="window.location.href=`inicio.php`" ><i class="fas fa-arrow-circle-left"></i></button>
-     <a href="cierre.php" class="btn btn-outline-secondary "> Cerrar Sesion</a>
+<div class="col-sm-6 col-xs-6 text-sm-right text-xs-center">
+    <a class="text-secondary" href="window.location.href=`inicio.php`" ><i class="fas fa-arrow-circle-left"></i></a>
+     <a href="cierre.php" class="text-secondary "> Cerrar Sesion</a>
 </div>
 </div>
 ';
