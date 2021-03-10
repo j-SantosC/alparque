b71b57a87f705721baa503118d21f158f0e4ab26
Jacobo Santos <jsantoscores@gmail.com>
2021-03-10 12:32:24 +0100

    subir imagen implementado

--- php/newmascotapost.php
+++ php/newmascotapost.php
@@ -14,13 +14,21 @@ include "conexion.php";
     $edad= $_POST["edad"];
     $sexo= $_POST["sexo"];
     $hora = $_POST["hora"];
-    $foto = $_POST["foto"];
     $descripcion = $_POST["descripcion"];
     $propietario = $_SESSION["usuario"];
+
+
+    $nombre_foto = $_FILES['foto']['name'];
+    $tipo_foto = $_FILES['foto']['type'];
+    $size_foto = $_FILES['foto']['size'];
+
+    $carpeta_destino = $_SERVER['DOCUMENT_ROOT'].'/img/mascotas/';
+
+    move_uploaded_file($_FILES['foto']['tmp_name'],$carpeta_destino.$nombre_imagen);
     
     
     $registrar ="INSERT INTO mascotas (nombre,edad,sexo,hora,img,descripcion,propietario,parque,date)
-                 VALUES ('$nombre','$edad','$sexo',0,'$foto','$descripcion','$propietario',0,'0000-00-00')";
+                 VALUES ('$nombre','$edad','$sexo',0,'foto','$descripcion','$propietario',0,'0000-00-00')";
     
  mysqli_query($conexion,$registrar);
  header("Location:inicio.php");
