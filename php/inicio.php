c54da5218cb6f85bd04154ed8b79b7197deab868
Jacobo Santos <jsantoscores@gmail.com>
2021-03-11 08:00:50 +0100

    opcion inicio con cero mascotas

--- php/inicio.php
+++ php/inicio.php
@@ -56,8 +56,18 @@
         <div class="container">
         <div class="row ">
             <div class="col text-center">
-               <h5 class=" mt-5 text-secondary font-italic">Como estan tus mascotas?</h5> 
-             
+            <?php
+                        $consultaInicio= " SELECT * FROM  mascotas WHERE propietario='" . $sesion . "'";
+                        
+                        $resultadosInicio=mysqli_query($conexion,$consultaInicio);
+                        if(mysqli_num_rows($resultadosInicio)==0){
+                            echo '<h5>¿Aun no has registrado a Tu perro?</h5>';
+                            echo '<a href="newmascota.php" class=" btn btn-outline-info btn-block p-3 btn">Registrar Mascota</a>';
+                        }  else{
+                            echo' <h5 class=" mt-5 text-secondary font-italic">Como estan tus mascotas?</h5> ';
+                        }
+        ?>
+        
             </div>
         </div>
     </div>
