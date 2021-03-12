7bccbde52679904c7727881da7768f7cc53b4187
Jacobo Santos <jsantoscores@gmail.com>
2021-03-12 08:22:41 +0100

    corrigiendo validacion forms

--- php/newmascota.php
+++ php/newmascota.php
@@ -84,7 +84,7 @@
             </form>
         </div>
     </div>
-    <script src="../js/newmascota4.js" type="text/javascript"></script>
+    <script src="../js/newmascota5.js" type="text/javascript"></script>
 </body>
 
 </html>
