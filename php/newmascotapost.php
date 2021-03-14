9abf8c434c6a67979a9f7b325b1b47d9907137ae
Jacobo Santos <jsantoscores@gmail.com>
2021-03-14 08:40:21 +0100

    añadiendo imagen por defecto

--- php/newmascotapost.php
+++ php/newmascotapost.php
@@ -22,19 +22,27 @@ include "conexion.php";
     $tipo_foto = $_FILES['foto']['type'];
     $size_foto = $_FILES['foto']['size'];
 
-    if($size_foto<=5000000){
+    if($nombre_foto){
+        if($size_foto<=5000000){
 
-    $carpeta_destino = $_SERVER['DOCUMENT_ROOT'].'/img/mascotas/';
-
-    $ruta_relativa = "../img/mascotas/";
-
-    move_uploaded_file($_FILES['foto']['tmp_name'],$carpeta_destino.$nombre_foto);
-
-    }  
-    
-    $registrar ="INSERT INTO mascotas (nombre,edad,sexo,hora,img,descripcion,propietario,parque,date)
-                 VALUES ('$nombre','$edad','$sexo',0,'$ruta_relativa$nombre_foto','$descripcion','$propietario',1,'0000-00-00')";
-    
+            $carpeta_destino = $_SERVER['DOCUMENT_ROOT'].'/img/mascotas/';
+        
+            $ruta_relativa = "../img/mascotas/";
+        
+            move_uploaded_file($_FILES['foto']['tmp_name'],$carpeta_destino.$nombre_foto);
+        
+            }  
+            
+            $registrar ="INSERT INTO mascotas (nombre,edad,sexo,hora,img,descripcion,propietario,parque,date)
+                         VALUES ('$nombre','$edad','$sexo',0,'$ruta_relativa$nombre_foto','$descripcion','$propietario',1,'0000-00-00')";
+            
+    }else{
+              
+            
+            $registrar ="INSERT INTO mascotas (nombre,edad,sexo,hora,img,descripcion,propietario,parque,date)
+                         VALUES ('$nombre','$edad','$sexo',0,'../img/defaultdog.jpeg','$descripcion','$propietario',1,'0000-00-00')";
+            
+    }
  mysqli_query($conexion,$registrar);
  header("Location:inicio.php");
 
