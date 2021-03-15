8a3dba0ebb7ccff5e04332b8c7cae4c81641f597
Jacobo Santos <jsantoscores@gmail.com>
2021-03-15 12:13:52 +0100

    importando funcionesval

--- js/funcionesval.js
+++ js/funcionesval.js
@@ -22,5 +22,36 @@ function eliminarError(e) {
     }
 }
 
+function verificarMail(e) {
 
-export { mostrarError, eliminarError }
+    const re = /^\S+@\S+\.\S+$/;
+
+    if (re.test(e.target.value)) {
+
+        e.target.classList.remove("is-invalid")
+        e.target.classList.add("is-valid")
+        vmail = true
+
+        activarBoton()
+
+        eliminarError(e)
+
+        mailhaserror = false;
+
+    } else {
+        e.target.classList.remove("is-valid")
+        e.target.classList.add("is-invalid")
+
+        vmail = false
+
+        desactivarBoton()
+
+        mostrarError(e, "El email no es correcto", mailhaserror)
+
+        mailhaserror = true;
+    }
+}
+
+
+
+export { mostrarError, eliminarError, verificarMail }
