20a8a82ed52a92f9af6d205fbca78853e9bede83
Jacobo Santos <jsantoscores@gmail.com>
2021-03-15 12:20:28 +0100

    importando funcionesval

--- js/editarusu.js
+++ js/editarusu.js
@@ -11,8 +11,37 @@ let mailhaserror = false;
 btnEnviar.disabled = true;
 
 
-email.addEventListener('blur', verificarMail(e, mailhaserror, vmail))
+email.addEventListener('blur', verificarMail)
 
+function verificarMail(e) {
+
+    const re = /^\S+@\S+\.\S+$/;
+
+    if (re.test(e.target.value)) {
+
+        e.target.classList.remove("is-invalid")
+        e.target.classList.add("is-valid")
+        vmail = true
+
+        activarBoton()
+
+        eliminarError(e)
+
+        mailhaserror = false;
+
+    } else {
+        e.target.classList.remove("is-valid")
+        e.target.classList.add("is-invalid")
+
+        vmail = false
+
+        desactivarBoton()
+
+        mostrarError(e, "El email no es correcto", mailhaserror)
+
+        mailhaserror = true;
+    }
+}
 
 function activarBoton() {
     if (vmail) {
--- js/funcionesval.js
+++ js/funcionesval.js
@@ -22,36 +22,4 @@ function eliminarError(e) {
     }
 }
 
-function verificarMail(e, mailhaserror, vmail) {
-
-    const re = /^\S+@\S+\.\S+$/;
-
-    if (re.test(e.target.value)) {
-
-        e.target.classList.remove("is-invalid")
-        e.target.classList.add("is-valid")
-        vmail = true
-
-        activarBoton()
-
-        eliminarError(e)
-
-        mailhaserror = false;
-
-    } else {
-        e.target.classList.remove("is-valid")
-        e.target.classList.add("is-invalid")
-
-        vmail = false
-
-        desactivarBoton()
-
-        mostrarError(e, "El email no es correcto", mailhaserror)
-
-        mailhaserror = true;
-    }
-}
-
-
-
-export { mostrarError, eliminarError, verificarMail }
+export { mostrarError, eliminarError }
