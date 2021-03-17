cc9fde9b4fe5bd8e9ea62a4c9d6fdbdb9221d918
Jacobo Santos <jsantoscores@gmail.com>
2021-03-17 22:06:40 +0100

    Trabajando con  sweetalert

--- php/mascotas.php
+++ php/mascotas.php
@@ -79,8 +79,8 @@ include "botones.php";
                     <td>$fila[0]</td> 
                     <td>$fila[1]</td>
                     <td>$fila[2]</td>
-                    <td><a data-value='$fila[0]' class='btn btn-danger btnBorrar'><i class='fa fa-trash text-white'></i></a>
-                    <button class='btn btn-warning'><a href='editarmascota.php?id=$fila[0]&nombre=$fila[1]&edad=$fila[2]&img=$fila[5]&descripcion=$fila[6]'><i class='fa fa-edit text-white'></i></button></td>
+                    <td><a data-value='$fila[0]' class='btn btn-danger btnBorrar'>Borrar</a>
+                    <button class='btn btn-warning'><a href='editarmascota.php?id=$fila[0]&nombre=$fila[1]&edad=$fila[2]&img=$fila[5]&descripcion=$fila[6]'>Editar</button></td>
                     </tr>";
                 }
 
