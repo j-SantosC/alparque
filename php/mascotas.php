c26f6a3d8a69cc3cfd052a6b9b85dc534f35ae79
Jacobo Santos <jsantoscores@gmail.com>
2021-03-17 11:35:06 +0100

    Trabajando con  sweetalert

--- php/mascotas.php
+++ php/mascotas.php
@@ -103,7 +103,7 @@ function estasSeguro(id) {
                     <td>$fila[1]</td>
                     <td>$fila[2]</td>
                     <td>$fila[3]</td>
-                    <td><button  class='btn btn-danger' onclick='estasSeguro($fila[0])'><i class='fa fa-trash text-white'></i></button>
+                    <td><button  class='btn btn-danger' onclick='estasSeguro(1)'><i class='fa fa-trash text-white'></i></button>
                     <button  class='btn btn-warning'><a href='editarmascota.php?id=$fila[0]&nombre=$fila[1]&edad=$fila[2]&img=$fila[5]&descripcion=$fila[6]'><i class='fa fa-edit text-white'></i></button></td>
                     </tr>";
                 }
