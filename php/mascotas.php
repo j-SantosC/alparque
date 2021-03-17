d95828facad67ed6b95fba08d809e7be7e2f10a3
Jacobo Santos <jsantoscores@gmail.com>
2021-03-17 10:51:56 +0100

    Trabajando con  sweetalert

--- php/mascotas.php
+++ php/mascotas.php
@@ -109,8 +109,8 @@ include "botones.php";
 
         </table>
     </div>
-</div>
+<!-- </div>
 <?php $v=rand(10000,99999)?>
- <script type="module" src="../js/mascotas.js?v=<?php echo $v ?>"></script>
+ <script type="module" src="../js/mascotas.js?v=<?php echo $v ?>"></script> -->
 </body>
 </html>
