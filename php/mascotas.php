78bfa8ddb89f4ee3d265127e66e03c646facd0b3
Jacobo Santos <jsantoscores@gmail.com>
2021-03-17 11:37:46 +0100

    Trabajando con  sweetalert

--- php/mascotas.php
+++ php/mascotas.php
@@ -28,32 +28,34 @@ include "botones.php";
 ?>
 <script type="module">
 
-    import Swal from '../node_modules/sweetalert2/src/sweetalert2.js'   
+    //import Swal from '../node_modules/sweetalert2/src/sweetalert2.js'   
 
 function estasSeguro() {
 
-    Swal.fire({
-        title: 'Estas Seguro?',
-        text: 'El email se modificara de forma definitiva',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Cambiar email'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            Swal.fire({
-                title: 'Buen Trabajo!',
-                text: 'Has cambiado tu email!',
-                icon: 'success',
-                showConfirmButton: false,
-            })
-            setTimeout(() => {
-                window.location(`../php/borrar.php?id=${id}`)
-            }, 1500)
+    console.log("hi bitch")
 
-        }
-    })
+    // Swal.fire({
+    //     title: 'Estas Seguro?',
+    //     text: 'El email se modificara de forma definitiva',
+    //     icon: 'warning',
+    //     showCancelButton: true,
+    //     confirmButtonColor: '#3085d6',
+    //     cancelButtonColor: '#d33',
+    //     confirmButtonText: 'Cambiar email'
+    // }).then((result) => {
+    //     if (result.isConfirmed) {
+    //         Swal.fire({
+    //             title: 'Buen Trabajo!',
+    //             text: 'Has cambiado tu email!',
+    //             icon: 'success',
+    //             showConfirmButton: false,
+    //         })
+    //         setTimeout(() => {
+    //             window.location(`../php/borrar.php?id=${id}`)
+    //         }, 1500)
+
+    //     }
+    // })
 }
 
 </script>
